7287396a26004931054ed631fd6cbc5afa4fb1e9
Max <tongweizj@gmail.com>
2025-04-11 21:59:22 -0400

    update

---
+++ Lab_code/week12/demo2/Test/IrisData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Test
+{
+    //IrisData 是输入数据类，其中包含数据集中每个特征的定义。
+    //使用 LoadColumn 属性指定数据集文件中源列的索引。
+    public class IrisData
+    {
+        [LoadColumn(0)]
+        public float SepalLength;
+
+        [LoadColumn(1)]
+        public float SepalWidth;
+
+        [LoadColumn(2)]
+        public float PetalLength;
+
+        [LoadColumn(3)]
+        public float PetalWidth;
+    }
+
+    //ClusterPrediction 类表示应用于 IrisData 实例的聚类模型的输出。
+    //使用 ColumnName 属性将 PredictedClusterId 和 Distances 字段分别绑定到 PredictedLabel 和 Score 列。
+    //对于聚类任​​务，这些列具有以下含义：
+    //PredictedLabel 列包含预测聚类的 ID。
+    //Score 列包含一个数组，其中包含到聚类质心的平方欧氏距离。数组长度等于聚类数量。
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[]? Distances;
+    }
+}
---
+++ Lab_code/week12/demo2/Test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using Microsoft.ML;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 使用数据生成模型
+            //string _modelPath = Path.Combine(Environment.CurrentDirectory, "data", "IrisClusteringModel.zip");
+            string _modelPath = Path.Combine(AppContext.BaseDirectory, "Data", "IrisClusteringModel.zip");
+            var mlContext = new MLContext(seed: 0);
+            
+
+            // 使用模型做预测
+            string featuresColumnName = "Features";
+            // pipeline
+            var pipeline = mlContext.Transforms
+                .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
+
+            // model, 数据按照pipeline设计的路径，生成model  
+            //var model = pipeline.Fit(dataView);
+            ITransformer model = mlContext.Model.Load(_modelPath, out _);
+
+            // 存model
+            //using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+            //{
+            //    mlContext.Model.Save(model, dataView.Schema, fileStream);
+            //}
+
+            // Use the model for predictions 模型生成预测
+            //PredictionEngine 是一个便捷的 API，可用于对单个数据实例执行预测。PredictionEngine 并非线程安全的。
+            //它适用于单线程或原型环境。
+            //为了在生产环境中提升性能和线程安全性，
+            //请使用 PredictionEnginePool 服务，该服务会创建一个包含 PredictionEngine 对象的 ObjectPool，供整个应用程序使用。
+            //请参阅本指南，了解如何在 ASP.NET Core Web API 中使用 PredictionEnginePool。
+            var predictor = mlContext.Model.CreatePredictionEngine<IrisData, ClusterPrediction>(model);
+
+            var prediction = predictor.Predict(TestIrisData.Setosa);
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances ?? Array.Empty<float>())}");
+
+        }
+    }
+}
